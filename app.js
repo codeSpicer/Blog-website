98de746b779f28c7c8847ed9aa403d89b2ab00a8
PAVILION\aksha <akshat.lko.2001@gmail.com>
2022-06-27 13:47:02 +0530

    Using mongoDB as a database for storing all the blog posts.

--- app.js
+++ app.js
@@ -2,67 +2,82 @@
 const express = require("express");
 const ejs = require("ejs");
 var _ = require("lodash");
+const mongoose = require("mongoose");
 
 // USING MODULES---------------------------------------------------------->
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect(
+  "mongodb+srv://akshat123:akshat123@cluster0.zeuj48k.mongodb.net/blog-site?retryWrites=true&w=majority"
+);
+
+//  MONGODB SETUP-------------------------------------------------------------------->
+const postSchema = new mongoose.Schema({ title: String, content: String });
+const Post = mongoose.model("Post", postSchema);
 
 // LISTENING TO SERVER--------------------------------------------------------------->
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
 
 // VARIABLES AND CONSTANTS------------------------------------------------------------>
 const homeStartingContent =
-    "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Doggo ipsum the neighborhood pupper heckin fat boi snoot, fat boi wow such tempt. You are doin me a concern length boy long woofer shoob fat boi puggorino lotsa pats, borkdrive length boy floofs fluffer wow very biscit. doggo corgo doggo. Very hand that feed shibe woofer wow very biscit long bois, wrinkler wow very biscit. Boof tungg the neighborhood pupper mlem, heck. Floofs extremely cuuuuuute puggorino pupperino puggorino, puggo the neighborhood pupper very jealous pupper clouds, porgo borking doggo aqua doggo.";
 const aboutContent =
-    "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+  "Doggo ipsum dat tungg tho adorable doggo you are doing me a frighten very taste wow pupper long bois sub woofer doggo, borkf wow such tempt thicc long bois clouds. Heckin good boys and girls long water shoob he made many woofs big ol pupper shooberino, tungg the neighborhood pupper. Boof pats the neighborhood pupper wow very biscit borking doggo shoober borkf, doggorino pats long water shoob super chub. adorable doggo doge clouds. Shibe fluffer blep wow very biscit wow such tempt, waggy wags pupper shoober. Long water shoob you are doing me a frighten clouds long water shoob maximum borkdrive blep dat tungg tho, super chub yapper mlem fat boi. Adorable doggo maximum borkdrive pupperino dat tungg tho much ruin diet lotsa pats big ol pupper, I am bekom fat fluffer floofs puggo.  floofs puggo very good spot. Long woofer shibe ruff, lotsa pats.";
 const contactContent =
-    "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
+  "Doggo ipsum vvv mlem clouds pupper fat boi long bois, blep fluffer fat boi smol. Maximum borkdrive extremely cuuuuuute sub woofer what a nice floof boofers puggorino, wrinkler very taste wow noodle horse corgo. Ruff floofs clouds ruff length boy ruff, stop it fren you are doing me the shock wow such tempt doge, big ol pupper very hand that feed shibe blop woofer. What a nice floof heckin good boys super chub blep blop very good spot shoob noodle horse vvv, shoob mlem the neighborhood pupper noodle horse doge heck boofers. Ruff noodle horse what a nice floof long bois waggy wags, I am bekom fat floofs.";
 
 // GET REQUESTS--------------------------------------------------------------->
 
 app.get("/", (req, res) => {
-    // console.log(posts[0].postContent, posts[0].title);
-    res.render("home", { content: homeStartingContent, posts: posts });
+  Post.find({}, (err, foundItems) => {
+    if (!err) {
+      res.render("home", { content: homeStartingContent, posts: foundItems });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", { content: aboutContent });
+  res.render("about", { content: aboutContent });
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact", { content: contactContent });
+  res.render("contact", { content: contactContent });
 });
 
 app.get("/compose", (req, res) => {
-    res.render("compose");
+  res.render("compose");
 });
 
-app.get("/posts/:postName", (req, res) => {
-    // console.log(req.params);
-    let postName = req.params.postName;
-    postName = _.lowerCase(postName);
+app.get("/posts/:postId", (req, res) => {
+  let postId = req.params.postId;
+  //   postname = _.lowerCase(postname);    // was used when we were using post name
 
-    posts.forEach((post) => {
-        // console.log("finding a valid match for ...", postName);
-        if (post.title == postName) {
-            // console.log("Match found!");
-            res.render("post", { post: post });
-        }
-    });
+  Post.findOne({ _id: postId }, (err, foundItems) => {
+    if (!err) {
+      res.render("post", { post: foundItems });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 // POST REQUESTS ------------------------------------------------------------->
 
 app.post("/compose", (req, res) => {
-    let post = {
-        title: req.body.title,
-        content: req.body.postContent,
-    };
-    posts.push(post);
-    res.redirect("/");
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.postContent,
+  });
+
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
